c7eaef9a4d24a0df00d093a978b97212c972783f
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-18 10:55:51 -0700

    updated outputs and added a new file

--- MathScripts/net_worth.js
+++ MathScripts/net_worth.js
@@ -4,6 +4,6 @@
 let assets = 100; //allowed to be changed 
 let debts =  80; //allowed to be changed 
 
-const netWorth = assets - debts;
+const netWorth = assets - debts; //formula for net worth
 
-console.log(netWorth.toFixed(2));
+console.log("Your net worth is " + netWorth.toFixed(2)); // calculation of net worth in decimals.

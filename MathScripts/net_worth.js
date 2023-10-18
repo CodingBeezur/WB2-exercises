5b50fd6c706b3c4f7feb2d4026944fcb3319f96e
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-18 10:44:03 -0700

    net_worth.js file done

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,9 @@
+// Creating a calculator to determine Net Worth
+// The formula would be assets - debts = Net Worth
+
+let assets = 100; //allowed to be changed 
+let debts =  80; //allowed to be changed 
+
+const netWorth = assets - debts;
+
+console.log(netWorth.toFixed(2));

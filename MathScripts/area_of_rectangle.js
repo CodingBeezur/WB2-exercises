bdba3dd1959b48245286561d2042b2a139cbfa96
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-18 10:53:14 -0700

    area_of_rectangle file done

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,10 @@
+// calculate the area of a rectangle 
+// formula for area of a rectangle is: length * width 
+
+let lengthOfRectangle = 5; //value of changed 
+let widthOfRectangle = 10; //value can be changed
+
+const areaOfRectangle = lengthOfRectangle * widthOfRectangle; //formula for the area of a rectangle. 
+
+console.log(areaOfRectangle); 
+console.log( "the area of the rectangle is: " + areaOfRectangle);

2e4212529a67a1105d7a3fd8519c11a64fd6a16e
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-18 14:17:47 -0700

    c_to_f file done

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,7 @@
+// convert celcius to fahrenheit 
+// the formula for celcius to fahrenheit is: F = (9/5)C + 32
+
+let celcius = 37.78; //allowed to be changed
+let fahrenheit =  ((9/5) * celcius )+ 32;
+
+console.log(fahrenheit.toFixed(2));
--- MathScripts/f_to_cs.js
+++ MathScripts/f_to_cs.js
@@ -1,7 +1,7 @@
 // convert Fahrenheit to Celcius 
 // formula of Fahrenheit to Celcius Formula 
 
-let Fahrenheit = 100 // Allowed to be changed
-let celcius = ((Fahrenheit -32) * 5/9);
+let fahrenheit = 100 // Allowed to be changed
+let celcius = ((fahrenheit -32) * 5/9);
 
 console.log(celcius.toFixed(2) + " celcius");

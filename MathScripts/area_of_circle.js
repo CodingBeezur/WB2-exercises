082d7d95d69ec7798888ce58426d34e2722d9211
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-18 11:31:27 -0700

    area_of_circle file finished

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,12 @@
+//calculate the area of a circle
+//the formula for the area of a circle is: π(radius)^2
+
+let radius = 5; //allowed to be changed 
+
+const pi = Math.PI; // value of pie
+
+const areaOfCircle = pi * (radius ** 2); //formula of area 
+
+
+console.log("The area of a circle with a radius of " + radius + " is " + areaOfCircle);
+

0a0545fbc5de54e85a4151519150aa02cbdbc83a
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-19 14:49:11 -0700

    added switchScripts Folder and new file

---
+++ IfScripts/show_major.js
@@ -0,0 +1,21 @@
+//define two variables for a student: studentName and studentMajor.
+//the studentMajor variable will contain major code. 
+
+// your script will look up students major code in the bale below and display the name of the major and location of the departmetnts office. 
+
+//variables 
+let studentName = "Cesar";
+let studentMajor = "CSCI";
+
+let computerScience = "Computer Science"; 
+let csLocation = "REMOTE"
+
+if (studentMajor == "ENG") {
+    console.log ("Student: " + studentName);
+    console.log ("Major is: " + english);
+    console.log("Advising Location: " + englishLocation );
+} else if (studentMajor == "CSCI") {
+    console.log("Student: " + studentName);
+    console.log ("Major is: " + computerScience);
+    console.log("Advising Location: " + csLocation );
+} 
